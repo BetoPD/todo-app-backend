c5ef4cb0fe45750f9b681ea2ec574b7512ddaa71
Humberto <humberto.angel.duran@gmail.com>
2024-01-31 19:40:24 -0600

    Add JSON validation middleware

--- src/app.js
+++ src/app.js
@@ -3,11 +3,21 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.routes.js';
 import tasksRouter from './routes/tasks.routes.js';
+import { validateJSON } from './middlewares/validateJSON.js';
 const app = express();
 
 // Adds a logger to the app
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use(express.json());
+
+// Error-handling middleware for JSON syntax errors
+app.use(validateJSON);
+
+app.use(cookieParser());
+app.use('/api', authRouter);
+app.use('/api', tasksRouter);
 app.use(cookieParser());
 app.use('/api', authRouter);
 app.use('/api', tasksRouter);
---
+++ src/middlewares/validateJSON.js
@@ -0,0 +1,6 @@
+export const validateJSON = (err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return res.status(400).json({ message: 'Bad JSON format' }); // Bad request
+  }
+  next();
+};
